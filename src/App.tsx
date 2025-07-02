d14fe43ae09192bd941f17ea6a9953bd1d58498b
Farzeen Qaiser <fiziqaiser@gmail.com>
2025-07-03 01:28:00 +0500

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import NavBar from './components/NavBar'
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import NavBar from './components/NavBar';
 
 function App() {
-
   return (
-    <>
-      <NavBar/>
-    </>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+
+      <Show above="lg">
+        <GridItem area="aside">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main">
+        Main Content
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
+import ColorModeSwitch from './ColorModeSwitch'
+
 import logo from '../assets/ditto_pfp.jpg'
 
 const NavBar = () => {
     return (
         <>
-            <HStack bg="blue.100" p={4}>
+            <HStack justifyContent='space-between' padding='10px'>
                 <Image src={logo} boxSize="60px"/>
-                <Text color="black">Ditto</Text>
+                <ColorModeSwitch/>
             </HStack>
         </>
     )
